90b2b322a9e1a260b54ed283fd32bc933f1791b5
Carlos <dela3499@gmail.com>
2014-07-25 10:49:52 -0500

    init - putting together gh-pages demo

---
+++ variable-react.js
@@ -0,0 +1,79 @@
+/** @jsx React.DOM */
+
+
+var average = function (myArray) {
+    var sum = 0;
+    for (var i = 0; i < myArray.length; i++) {
+        sum += myArray[i];
+    };
+    return sum / myArray.length;
+};
+
+var saturate = function (value,limits) {
+    var min = Math.min(limits[0],limits[1]);
+    var max = Math.max(limits[0],limits[1]);
+    if (value < min) {
+        return min;
+    } else if (value > max) {
+        return max;
+    };
+    return value;
+};
+
+var Variable = React.createClass({
+    /*
+    Required Properties: 
+     limits (array with two element: [minimum value and maximum value]
+    
+    Optional Properties: 
+     callback (calls this function on every state change, with current value as argument)
+     init
+     dragRange (mouse covers entire range of values after moving this many pixels)
+     format  (a function to prepare numerical value for display - e.g. rounding, date/time formatting, currency)
+    
+    Notes: 
+     1. this component produces a <span> element with a class called "variable" (use this for styling)
+    */
+    getInitialState: function () {
+        return {value: this.props.init || average(this.props.limits), 
+                dragging: false};
+    },
+    componentDidUpdate: function () {
+        f = this.props.callback;
+        if (f) {f(this.state.value)};
+    },
+    componentDidMount: function (root) {
+        var t = this;
+        $(document).mousemove(function (e) {
+            if (t.state.dragging) {
+                var range = t.props.limits[1] - t.props.limits[0],
+                    dragRange = t.props.dragRange || 100,
+                    mouseY = e.clientY || e.pageY,
+                    value = saturate(t.state.startVal + (range)*((mouseY - t.state.mouseY0)/dragRange), t.props.limits);
+                t.setState({value: value});
+            };
+        });
+    },
+    handleMouseDown: function (e) {
+        var component = this;
+        this.setState({dragging: true, 
+                       startVal: this.state.value,
+                       mouseY0: (e.clientY || e.pageY)});
+        $("body").css({cursor: "ns-resize"});
+        $(document).one("mouseup", function () {
+            component.setState({dragging: false});
+            $("body").css({cursor: ""});
+        });
+    },
+    handleMouseOver: function (e) {
+        $(e.target).css({cursor:"ns-resize"});
+    },
+    render: function () {
+        var format = this.props.format || function (x) {return x};
+        return (
+            <span onMouseDown={this.handleMouseDown} onMouseOver={this.handleMouseOver}>
+            {format(this.state.value)}
+            </span>
+        );
+    }
+});
